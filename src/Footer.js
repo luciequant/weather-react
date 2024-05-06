8bfc70cad373bb2837eab89aed8fb552f7003efe
luciequant <156855912+luciequant@users.noreply.github.com>
2024-05-06 17:46:48 +0200

    Debug

--- src/Footer.js
+++ src/Footer.js
@@ -4,12 +4,16 @@ export default function Footer() {
   return (
     <footer>
       This project was coded by
-      <a href="https://github.com/luciequant" target="_blank">
+      <a href="https://github.com/luciequant" rel="noopener" target="_blank">
         {" "}
         Lucie Quantová
       </a>
       , is open-sourced on
-      <a href="https://github.com/luciequant/weather-react" target="_blank">
+      <a
+        href="https://github.com/luciequant/weather-react"
+        rel="noopener"
+        target="_blank"
+      >
         {" "}
         GitHub
       </a>{" "}

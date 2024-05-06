7f4b84b654e6d5a0bd42786a4ca0dabd72b1fd88
luciequant <156855912+luciequant@users.noreply.github.com>
2024-05-06 17:05:07 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
 import Weather from "./Weather.js";
+import Footer from "./Footer.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Weather App</h1>
-        <Weather></Weather>
+        <Weather />
+        <Footer />
       </header>
     </div>
   );
---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+export default function Footer() {
+  return (
+    <footer>
+      This project was coded by
+      <a href="https://github.com/luciequant" target="_blank">
+        {" "}
+        Lucie Quantová
+      </a>
+      , is open-sourced on
+      <a href="https://github.com/luciequant/weather-react" target="_blank">
+        {" "}
+        GitHub
+      </a>{" "}
+    </footer>
+  );
+}
